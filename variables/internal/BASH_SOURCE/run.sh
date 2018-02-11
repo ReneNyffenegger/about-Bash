b9f5d9118aa21e900b471c757afb4ffcebcceb6b
René Nyffenegger <rene.nyffenegger@adp-gmbh.c>
2018-02-11 18:04:33 +0100

    + BASH_SOURCE

---
+++ variables/internal/BASH_SOURCE/run.sh
@@ -0,0 +1,5 @@
+. ./tq84_func.sh
+
+tq84_func
+
+tq84_call_func
---
+++ variables/internal/BASH_SOURCE/tq84_func.sh
@@ -0,0 +1,13 @@
+tq84_func() {
+
+  echo "BASH_SOURCE[0]: ${BASH_SOURCE[0]}"
+  echo "BASH_SOURCE[1]: ${BASH_SOURCE[1]}"
+  echo "BASH_SOURCE[2]: ${BASH_SOURCE[2]}"
+
+}
+
+tq84_call_func() {
+
+  tq84_func
+
+}
